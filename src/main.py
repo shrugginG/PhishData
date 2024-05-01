d31a8af8b14482efbcab48f20ea4733450b4b869
shrugginG <1245280461@qq.com>
2024-05-01 13:08:54 +0800

    Update github action yml file to use secerts

--- src/main.py
+++ src/main.py
@@ -1,23 +1,31 @@
 import os
+import argparse
 import pymysql
 
 from utils.phishing_intelligence.fetch_openphish import get_openphish_feed
 from utils.mysql_utils import fetch_result
 
+def parse_args():
+    parser = argparse.ArgumentParser(description="Fetch github action secerets")
+    parser.add_argument('--host', required=True, help='Database host')
+    parser.add_argument('--user', required=True, help='Database user')
+    parser.add_argument('--password', required=True, help='Database password')
+    parser.add_argument('--port', type=int, required=True, help='Database port')
+    return parser.parse_args()
+
 
 if __name__ == "__main__":
     # get_openphish_feed()
-    print(
-        os.getenv('MYSQL_HOST'),
-        os.getenv('MYSQL_USER'),
-        os.getenv('MYSQL_PASSWORD'),
-        os.getenv('MYSQL_PORT')
-    )
-    # mysql_conn = pymysql.connect(
-    #     host=os.getenv('MYSQL_HOST'),
-    #     user=os.getenv('MYSQL_USER'),
-    #     password=os.getenv('MYSQL_PASSWORD'),
-    #     port=os.getenv('MYSQL_PORT'),
-    #     cursorclass=pymysql.cursors.DictCursor
-    # ) 
-    # print(fetch_result(mysql_conn,'select * from benign.benign_urls limit 1'))
+
+    # Parse arguments
+    args = parse_args()
+
+    mysql_conn = pymysql.connect(
+        host=args.host,
+        user=args.user,
+        password=args.password,
+        port=args.port,
+        cursorclass=pymysql.cursors.DictCursor
+    ) 
+    
+    print(fetch_result(mysql_conn,'select * from benign.benign_urls limit 1'))
