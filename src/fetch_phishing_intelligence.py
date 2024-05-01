a2c90f15c17263739e99b7e45b7b7f7cd809c435
shrugginG <1245280461@qq.com>
2024-05-01 18:15:12 +0800

    Test github action

---
+++ src/fetch_phishing_intelligence.py
@@ -0,0 +1,27 @@
+def main():
+
+    # Fetch openphish phishing intelligence
+
+
+
+    # TODO - Fetch phishtank phishing intelligence
+
+    # TODO - Fetch eCrimeX phishing intelligence
+
+    # TODO - Fetch urlhaus phishing intelligence
+
+    # TODO - Fetch certstream phishing intelligence
+
+    # TODO - Fetch phishstats phishing intelligence
+
+    # TODO - Fetch urlscan phishing intelligence
+
+    # TODO - Fetch phishlabs phishing intelligence
+
+
+
+
+
+if __name__ == "__main__":
+    print(1)
+    main()
--- src/main.py
+++ src/main.py
@@ -1,16 +1,13 @@
-import os
 import argparse
-import pymysql
 
-from utils.phishing_intelligence.fetch_openphish import get_openphish_feed
-from utils.mysql_utils import fetch_result
+from modules.phishing_intelligence.fetch_openphish import fetch_openphish_intelligence
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Fetch github action secerets")
-    parser.add_argument('--host', required=True, help='Database host')
-    parser.add_argument('--user', required=True, help='Database user')
-    parser.add_argument('--password', required=True, help='Database password')
-    parser.add_argument('--port', type=int, required=True, help='Database port')
+    parser.add_argument("--host", required=True, help="Database host")
+    parser.add_argument("--user", required=True, help="Database user")
+    parser.add_argument("--password", required=True, help="Database password")
+    parser.add_argument("--port", type=int, required=True, help="Database port")
     return parser.parse_args()
 
 
@@ -20,12 +17,4 @@ if __name__ == "__main__":
     # Parse arguments
     args = parse_args()
 
-    mysql_conn = pymysql.connect(
-        host=args.host,
-        user=args.user,
-        password=args.password,
-        port=args.port,
-        cursorclass=pymysql.cursors.DictCursor
-    ) 
-    
-    print(fetch_result(mysql_conn,'select * from benign.benign_urls limit 1'))
+    fetch_openphish_intelligence(args)
