428d17fc89b40b37fbbdf603c0645f359c10269b
shrugginG <1245280461@qq.com>
2024-05-01 12:50:18 +0800

    Test again

--- src/main.py
+++ src/main.py
@@ -7,11 +7,17 @@ from utils.mysql_utils import fetch_result
 
 if __name__ == "__main__":
     # get_openphish_feed()
-    mysql_conn = pymysql.connect(
-        host=os.getenv('MYSQL_HOST'),
-        user=os.getenv('MYSQL_USER'),
-        password=os.getenv('MYSQL_PASSWORD'),
-        port=os.getenv('MYSQL_PORT'),
-        cursorclass=pymysql.cursors.DictCursor
-    ) 
-    print(fetch_result(mysql_conn,'select * from benign.benign_urls limit 1'))
+    print(
+        os.getenv('MYSQL_HOST'),
+        os.getenv('MYSQL_USER'),
+        os.getenv('MYSQL_PASSWORD'),
+        os.getenv('MYSQL_PORT')
+    )
+    # mysql_conn = pymysql.connect(
+    #     host=os.getenv('MYSQL_HOST'),
+    #     user=os.getenv('MYSQL_USER'),
+    #     password=os.getenv('MYSQL_PASSWORD'),
+    #     port=os.getenv('MYSQL_PORT'),
+    #     cursorclass=pymysql.cursors.DictCursor
+    # ) 
+    # print(fetch_result(mysql_conn,'select * from benign.benign_urls limit 1'))
